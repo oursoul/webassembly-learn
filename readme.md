4edd29b46dc026dd9804f4cc58b6587c92ca2b0b
oursoul <2335462510@qq.com>
2021-12-23 21:39:35 +0800

    first try wasm in web

---
+++ readme.md
@@ -0,0 +1,5 @@
+## 首次在前端页面使用webassembly
+
+首先编写需要的C++代码，使用[工具](https://mbebenita.github.io/WasmExplorer/)转化成<b>assembly</b>，点击down
+
+然后在前端页面中使用，（使用vscode开一个Live服务器访问assembly文件）
---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int fib(int x){
+  if(x<=0)return 0;
+  if(x<=2)return 1;
+  return fib(x-1)+fib(x-2);
+}
+int main(){
+  int t1,t2;
+  t1 = clock();
+  fib(4);
+  t2 = clock();
+  cout<< t2-t1 << "ms" <<endl;
+}
